097cad9931f482f9e3059c7dffe8e551190748e9
godaminator02 <guruprasathjay@gmail.com>
2024-01-22 13:16:33 +0530

    custom button added

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -4,42 +4,41 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:zoom_app/utils/utils.dart';
 
-class AuthMethods{
+class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore_firestore = FirebaseFirestore.instance;
 
-  Future<bool>signInWithGoogle(BuildContext context) async{
+  Future<bool> signInWithGoogle(BuildContext context) async {
     bool res = false;
-    try{
+    try {
       final GoogleSignInAccount? googleuser = await GoogleSignIn().signIn();
 
-      final GoogleSignInAuthentication? googleAuth = await googleuser?.authentication;
+      final GoogleSignInAuthentication? googleAuth =
+          await googleuser?.authentication;
 
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
 
-      UserCredential userCredential = await _auth.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
 
       User? user = userCredential.user;
 
-      if(user!=null){
-        if(userCredential.additionalUserInfo!.isNewUser){
+      if (user != null) {
+        if (userCredential.additionalUserInfo!.isNewUser) {
           await _firestore.collection('users').doc(user.uid).set({
             'username': user.displayName,
             'uid': user.uid,
             'profilePhoto': user.photoURL,
-
-            });
+          });
         }
         res = true;
       }
-
-    }on FirebaseAuthException catch(e){
+    } on FirebaseAuthException catch (e) {
       showsnackBar(context, e.message!);
       res = false;
-
     }
     return res;
   }
